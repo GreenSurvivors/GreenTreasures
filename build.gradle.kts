b5fef3170710d19634c7a55765372a5c707cbf41
FireInstall <feuerreiter@greensurvivors.de>
2024-12-04 17:21:04 +0100

    update to Mc 1.21.4 and gradle 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,21 @@
 plugins {
     `java-library`
     //java
-    id("io.papermc.paperweight.userdev") version "1.5.9" // plugin for developing Paper plugins using server internals
-    id("xyz.jpenilla.run-paper") version "2.2.0" // Adds runServer and runMojangMappedServer tasks for testing
+    id("io.papermc.paperweight.userdev") version "1.7.3"
+    id("xyz.jpenilla.run-paper") version "2.3.0"
 }
 
 group = "de.greensurvivors"
 version = "0.1.2-SNAPSHOT"
 description = "Like TreasureChest but with less bugs!"
-//java.sourceCompatibility = JavaVersion.VERSION_17
+val mcVersion by extra("1.21.4")
+
+// we only work with paper and downstream!
+paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 repositories {
@@ -32,7 +35,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.20.2-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("$mcVersion-R0.1-SNAPSHOT")
     //get prebuilt from jitpack.io
     //compileOnly("com.github.HenrikoMagnifico","TreasureChestX","8dbc187c09")
     //local installation in case jitpack doesn't work
@@ -40,29 +43,17 @@ dependencies {
 }
 
 tasks {
-  // Configure reobfJar to run when invoking the build task
-  assemble {
-    dependsOn(reobfJar)
-  }
-
   compileJava {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
 
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(17)
+      options.release.set(21)
   }
   
   processResources {
     filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-  }
 
-  /*
-  reobfJar {
-    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
+      expand(project.properties)
   }
-   */
- 
 }
